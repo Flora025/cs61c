f0896ac5e31e8f69dde4a5d985730fcacd0d1a49
Flora025 <2365349265@qq.com>
2023-06-25 22:53:44 +0800

    fin lab01

--- labs/lab01/ll_cycle.c
+++ labs/lab01/ll_cycle.c
@@ -1,7 +1,22 @@
 #include <stddef.h>
 #include "ll_cycle.h"
 
-int ll_has_cycle(node *head) {
+int ll_has_cycle(node *head)
+{
     /* your code here */
+    node *tortoise = head;
+    node *hare = head;
+    // 如果快指针.next是null，说明已经到达末尾，即no cycle
+    while (tortoise != NULL && hare != NULL && hare->next != NULL)
+    {
+        // 1. 前进指针
+        tortoise = tortoise->next; // 不需要地址&
+        hare = hare->next->next;
+        // 2. 判断是否会相遇
+        if (tortoise == hare)
+        {
+            return 1;
+        }
+    }
     return 0;
 }
