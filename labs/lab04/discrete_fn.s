85408b90ee772c0b33fdaf69001273caa8fc2a38
Flora025 <2365349265@qq.com>
2023-08-02 12:48:17 +0800

    fin lab4

--- labs/lab04/discrete_fn.s
+++ labs/lab04/discrete_fn.s
@@ -77,7 +77,10 @@ main:
 # Think: why might having a1 be useful?
 f:
     # YOUR CODE GOES HERE!
-
+    addi t0, a0, 3 # offset
+    slli t0, t0, 2
+    add t1, t0, a1 # address of cur output
+    lw a0, 0(t1)
     jr ra               # Always remember to jr ra after your function!
 
 print_int:
